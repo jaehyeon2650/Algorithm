4decf6ddc14f6d4d8db156f45c5baa83adc923fc
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2024-07-19 22:23:35 +0900

    [Bronze V] Title: A+B, Time: 216 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+    public static int n;
+    public static int m;
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        n=scan.nextInt();
+        m=scan.nextInt();
+        solution();
+    }
+    public static void solution(){
+        System.out.println(n+m);
+    }
+}
