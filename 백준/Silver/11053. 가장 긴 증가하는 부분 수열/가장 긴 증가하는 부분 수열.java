b9c031c6bd4805051a163890901a698e5f407122
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2024-08-30 12:43:20 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 104 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,36 +1,30 @@
-import java.io.*;
-import java.util.*;
+    import java.io.*;
+    import java.util.Arrays;
 
-class Main {
+    class Main {
+        public static int n;
+        public static int[] a;
+        public static int[] result;
 
-    public static int n;
-    public static int[] a;
-    public static int[] dp;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        n=Integer.parseInt(bf.readLine());
-        a=new int[n];
-        dp=new int[n];
-        String s=bf.readLine();
-        String[] strings = s.split(" ");
-        for(int i=0;i<n;i++){
-            a[i]=Integer.parseInt(strings[i]);
-        }
-        for(int i=0;i<n;i++){
-            dp[i]=1;
-            int minn=dp[0];
-            for(int j=0;j<i;j++){
-                if(a[i]>a[j]&&minn<=dp[j]){
-                    dp[i]=dp[j]+1;
-                    minn=dp[j];
+        public static void main(String[] args) throws IOException {
+            BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+            n = Integer.parseInt(bf.readLine());
+            String s = bf.readLine();
+            String[] strings = s.split(" ");
+            a = new int[n];
+            result = new int[n];
+            Arrays.fill(result,Integer.MAX_VALUE);
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(strings[i]);
+            }
+            int maxLength=0;
+            for(int i=0;i<n;i++){
+                int ind = Arrays.binarySearch(result, 0, maxLength + 1, a[i]);
+                if(ind<0){
+                    result[-ind-1]=a[i];
+                    maxLength=Math.max(maxLength,-ind-1);
                 }
             }
+            System.out.println(maxLength+1);
         }
-        int minn=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            minn=Math.max(minn,dp[i]);
-        }
-        System.out.println(minn);
     }
-}
