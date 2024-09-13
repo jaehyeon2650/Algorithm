c65531a1850eaf28488df2a5be7da7aa53a08628
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2024-09-13 11:19:23 +0900

    [Gold II] Title: 경로 찾기, Time: 272 ms, Memory: 50468 KB -BaekjoonHub

--- 백준/Gold/1513. 경로 찾기/경로 찾기.java
+++ 백준/Gold/1513. 경로 찾기/경로 찾기.java
@@ -17,6 +17,13 @@
             m=Integer.parseInt(s1[1]);
             c=Integer.parseInt(s1[2]);
             a=new int[n+1][m+1];
+            for(int i=1;i<=c;i++){
+                s=br.readLine();
+                s1=s.split(" ");
+                int y=Integer.parseInt(s1[0]);
+                int x=Integer.parseInt(s1[1]);
+                a[y][x]=i;
+            }
             for(int i=0;i<51;i++){
                 for(int j=0;j<51;j++){
                     for(int k=0;k<51;k++){
@@ -24,17 +31,8 @@
                     }
                 }
             }
-
-            for(int i=1;i<=c;i++){
-                s=br.readLine();
-                s1=s.split(" ");
-                int y,x;
-                y=Integer.parseInt(s1[0]);
-                x=Integer.parseInt(s1[1]);
-                a[y][x]=i;
-            }
             for(int i=0;i<=c;i++){
-                stringBuffer.append( go(1,1,i,0)+" ");
+                stringBuffer.append(go(1,1,i,0)+" ");
             }
             System.out.println(stringBuffer);
         }
@@ -42,19 +40,20 @@
         public static int go(int y,int x,int cnt,int cur){
             if(y>n||x>m) return 0;
             if(y==n&&x==m){
-                if(cnt==0&&a[y][x]==0) return 1;
-                if(cnt==1&&a[y][x]>cur) return 1;
+                if(a[y][x]>0&&a[y][x]>cur&&cnt==1) return 1;
+                else if(a[y][x]==0&&cnt==0) return 1;
                 return 0;
             }
             if(dp[y][x][cnt][cur]!=-1) return dp[y][x][cnt][cur];
             int ret=0;
             if(a[y][x]==0){
                 ret=(go(y+1,x,cnt,cur)+go(y,x+1,cnt,cur))%1000007;
-            }else if(a[y][x]>cur&&cnt>=1){
+            }else if(a[y][x]>0&&cnt>0&&a[y][x]>cur){
                 ret=(go(y+1,x,cnt-1,a[y][x])+go(y,x+1,cnt-1,a[y][x]))%1000007;
             }
             dp[y][x][cnt][cur]=ret;
             return ret;
         }
+
 
     }
