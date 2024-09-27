64cba6c5de034f5bd537601daf301b826737fc63
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2024-09-27 13:23:11 +0900

    [Gold V] Title: 합분해, Time: 124 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -7,30 +7,23 @@ public class Main {
     public static int[][] dp=new int[204][204];
     public static void main(String[] args) throws IOException {
         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-        for(int i=0;i<204;i++){
-            Arrays.fill(dp[i],-1);
-        }
         String s=bf.readLine();
         String[] s1 = s.split(" ");
         n=Integer.parseInt(s1[0]);
         k=Integer.parseInt(s1[1]);
-        System.out.println(go(0,0));
-
-    }
-    public static int go(int cnt,int sum){
-        if(cnt==k){
-            if(sum==n) return 1;
-            return 0;
-        }
-        if(dp[cnt][sum]!=-1) return dp[cnt][sum];
-        int ret=0;
+        dp[0][0]=1;
         for(int i=0;i<=n;i++){
-            if(sum+i<=n){
-                ret+=go(cnt+1,sum+i);
-                ret%=1000000000;
+            dp[1][i]=1;
+        }
+        for(int i=2;i<=k;i++){
+            for(int j=0;j<=n;j++){
+                for(int k=0;k<=j;k++){
+                    dp[i][j]+=dp[i-1][j-k];
+                    dp[i][j]%=1000000000;
+                }
             }
         }
-        dp[cnt][sum]=ret;
-        return ret;
+        System.out.println(dp[k][n]);
     }
+
 }
