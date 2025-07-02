635f8a2e8fc38ff7ba55dca9bef286993f7c592a
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-07-02 20:22:51 +0900

    [Platinum V] Title: Strongly Connected Component, Time: 500 ms, Memory: 50972 KB -BaekjoonHub

--- 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.java
+++ 백준/Platinum/2150. Strongly Connected Component/Strongly Connected Component.java
@@ -1,142 +1,105 @@
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Stack;
-import java.util.Vector;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
-
-    static class Pair implements Comparable<Pair> {
-        int x;
-        int y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public int compareTo(Pair o) {
-            return x - o.x;
-        }
-    }
-
-    private static int n;
-    private static int m;
-    private static Vector<Vector<Integer>> arr = new Vector<>();
-    private static Vector<Vector<Integer>> arr2 = new Vector<>();
-    private static int[] visited;
-    private static Stack<Integer> stack = new Stack<>();
-    private static int total = 0;
+    public static int n;
+    public static int m;
+    public static List<List<Integer>> graph = new ArrayList<>();
+    public static List<List<Integer>> reverseGraph = new ArrayList<>();
+    public static List<List<Integer>> result = new ArrayList<>();
+    public static Map<Integer,Integer> tree = new TreeMap<>();
+    public static Stack<Integer> stack = new Stack<>();
+    public static int[] visited;
+    public static int total = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        init();
 
-        String token = bf.readLine();
-        String[] split = token.split(" ");
-        n = Integer.parseInt(split[0]);
-        m = Integer.parseInt(split[1]);
-        initialize();
-        fill(bf);
-
-        for(int i=1;i<=n;i++){
-            dfs1(i);
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            graph.get(v1).add(v2);
+            reverseGraph.get(v2).add(v1);
         }
 
-        clearVisited();
-        reverse();
-        clearArr();
-        goStack();
-        System.out.println(total);
-        for (int i = 1; i < visited.length; i++) {
-            arr.get(visited[i]).add(i);
-        }
-        List<Pair> result = new ArrayList<>();
-        for (int i = 0; i < arr.size(); i++) {
-            if (!arr.get(i).isEmpty()) {
-                result.add(new Pair(arr.get(i).get(0), i));
+        for (int i = 1; i <= n; i++) {
+            if (visited[i] == 0) {
+                dfs1(i);
             }
         }
-        Collections.sort(result);
-        for (Pair pair : result) {
-            for (int i = 0; i < arr.get(pair.y).size(); i++) {
-                System.out.print(arr.get(pair.y).get(i) + " ");
+
+        visited = new int[n + 1];
+
+        while (!stack.isEmpty()) {
+            int top = stack.pop();
+            if(visited[top]==0){
+                total++;
+                dfs2(top,top);
+                Collections.sort(result.get(top));
+                tree.put(result.get(top).get(0),top);
             }
-            System.out.println("-1");
         }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(total+"\n");
+
+        for (Integer value : tree.values()) {
+            for (Integer re : result.get(value)) {
+                sb.append(re+" ");
+            }
+            sb.append("-1\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
     }
 
-    private static void fill(BufferedReader bf) throws IOException {
-        for (int i = 0; i < m; i++) {
-            String token1 = bf.readLine();
-            String[] split1 = token1.split(" ");
-            int v1 = Integer.parseInt(split1[0]);
-            int v2 = Integer.parseInt(split1[1]);
-            arr.get(v1).add(v2);
+    private static void init() {
+        visited = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+            reverseGraph.add(new ArrayList<>());
+            result.add(new ArrayList<>());
         }
     }
 
     private static void dfs1(int x) {
-        if (visited[x] != 1) {
-            stack.add(x);
-            visited[x] = 1;
-            for (int i = 0; i < arr.get(x).size(); i++) {
-                if (visited[arr.get(x).get(i)] == 0) {
-                    dfs1(arr.get(x).get(i));
-                }
-            }
-            stack.add(x);
-        }
-    }
-
-    private static void dfs2(int x, int start) {
         if (visited[x] == 0) {
-            visited[x] = start;
-            for (int i = 0; i < arr2.get(x).size(); i++) {
-                if (visited[arr2.get(x).get(i)] == 0) {
-                    dfs2(arr2.get(x).get(i), start);
+            visited[x] = 1;
+            for (int i = 0; i < graph.get(x).size(); i++) {
+                if (visited[graph.get(x).get(i)] == 0) {
+                    dfs1(graph.get(x).get(i));
                 }
             }
+            stack.add(x);
         }
     }
 
-    private static void goStack() {
-        while (!stack.empty()) {
-            int top = stack.pop();
-            if (visited[top] == 0) {
-                total++;
-                dfs2(top, top);
+    private static void dfs2(int x, int group) {
+        if (visited[x] == 0) {
+            visited[x] = 1;
+            result.get(group).add(x);
+            for (int i = 0; i < reverseGraph.get(x).size(); i++) {
+                if(visited[reverseGraph.get(x).get(i)]==0){
+                    dfs2(reverseGraph.get(x).get(i),group);
+                }
             }
-        }
-    }
-
-    private static void reverse() {
-        for (int i = 1; i < arr.size(); i++) {
-            for (int j = 0; j < arr.get(i).size(); j++) {
-                arr2.get(arr.get(i).get(j)).add(i);
-            }
-        }
-    }
-
-    private static void initialize() {
-        clearVisited();
-        for (int i = 0; i <= n; i++) {
-            arr.add(new Vector<>());
-            arr2.add(new Vector<>());
-        }
-    }
-
-    private static void clearVisited() {
-        visited = new int[n + 1];
-    }
-
-    private static void clearArr() {
-        for (int i = 0; i < arr.size(); i++) {
-            arr.get(i).clear();
         }
     }
 }
