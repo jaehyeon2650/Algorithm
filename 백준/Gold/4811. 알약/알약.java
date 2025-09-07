98d3be228c26d07be73eaedf1cf3ce39a39a6ed6
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-09-07 13:27:33 +0900

    [Gold V] Title: 알약, Time: 228 ms, Memory: 29760 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.java
+++ 백준/Gold/4811. 알약/알약.java
@@ -1,26 +1,44 @@
-    import java.io.*;
-    import java.math.BigDecimal;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.Arrays;
 
-    class Main {
+public class Main {
 
-        public static BigDecimal[][][] dp=new BigDecimal[61][31][31];
+    private static BigInteger[][] dp = new BigInteger[31][31];
+    private static int n;
 
-        public static void main(String[] args) throws IOException {
-            for(int i=1;i<=30;i++){
-                go(i*2,i,0);
-            }
-            BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-            int num=Integer.parseInt(bf.readLine());
-            while(num!=0){
-                System.out.println(dp[num*2][num][0]);
-                num=Integer.parseInt(bf.readLine());
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        while ((n = Integer.parseInt(bf.readLine())) != 0) {
+            initialize();
+            System.out.println(go(n,0));
         }
-        public static BigDecimal go(int index,int hNum,int wNum){
-            if(hNum<0||wNum<0) return BigDecimal.ZERO;
-            if(index==0) return BigDecimal.ONE;
-            if(dp[index][hNum][wNum]!=null) return dp[index][hNum][wNum];
-            dp[index][hNum][wNum]=go(index-1,hNum-1,wNum+1).add(go(index-1,hNum,wNum-1));
-            return dp[index][hNum][wNum];
+
+    }
+
+    private static void initialize() {
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], BigInteger.valueOf(-1));
         }
     }
+
+    private static BigInteger go(int one, int half){
+        if(one<0||half<0) return BigInteger.ZERO;
+        if(one==0&&half==0) return BigInteger.ONE;
+        if(!dp[one][half].equals(BigInteger.valueOf(-1))) return dp[one][half];
+
+        BigInteger total = BigInteger.ZERO;
+        total = total.add(go(one-1,half+1));
+        if(half!=0){
+            total = total.add(go(one,half-1));
+        }
+
+        dp[one][half]=total;
+
+        return total;
+    }
+}
+
+
