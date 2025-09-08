4bf92541213285460b26e3db326883ad2a342010
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-09-08 10:25:49 +0900

    [Gold IV] Title: 사탕 가게, Time: 676 ms, Memory: 303068 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.java
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.java
@@ -1,39 +1,55 @@
-    import java.io.*;
-    import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
-    class Main {
+public class Main {
 
-        public static int n;
-        public static int k;
-        public static int[] dp=new int[10004];
-        public static void main(String[] args) throws IOException {
-            BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-            StringBuffer sb=new StringBuffer();
-            String s = bf.readLine();
-            String[] strings = s.split(" ");
-            n=Integer.parseInt(strings[0]);
-            k=(int) (Double.parseDouble(strings[1])*100+0.5);
-            while (n!=0&&k!=0){
-                Arrays.fill(dp,0);
-                int minn=0;
-                for(int i=0;i<n;i++){
-                    s = bf.readLine();
-                    strings = s.split(" ");
-                    int a=Integer.parseInt(strings[0]);
-                    int b=(int) (Double.parseDouble(strings[1])*100+0.5);
-                    for(int j=b;j<=k;j++){
-                        if(dp[j]<dp[j-b]+a){
-                            dp[j]=dp[j-b]+a;
-                            minn=Math.max(minn,dp[j]);
-                        }
-                    }
-                }
-                sb.append(minn+"\n");
-                s = bf.readLine();
-                strings = s.split(" ");
-                n=Integer.parseInt(strings[0]);
-                k=(int) (Double.parseDouble(strings[1])*100+0.5);
-            }
-            System.out.println(sb);
+    static class Candy {
+        int cal;
+        int price;
+
+        public Candy(final int cal, final double price) {
+            this.cal = cal;
+            this.price = (int) (price*100+0.5);
         }
     }
+
+    private static int n = -1;
+    private static int money = -1;
+    private static List<Candy> candies = new ArrayList<>();
+    private static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb =new StringBuffer();
+        while (true) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            candies.clear();
+            n = Integer.parseInt(st.nextToken());
+            money = (int)(Double.parseDouble(st.nextToken())*100+0.5);
+            if(n==0 && money==0) break;
+            dp = new int[(int)(money*100)];
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(bf.readLine());
+                int cal = Integer.parseInt(st.nextToken());
+                double money = Double.parseDouble(st.nextToken());
+                candies.add(new Candy(cal, money));
+            }
+            int maxn = 0;
+            for(int i=0;i<candies.size();i++){
+                for(int j = candies.get(i).price;j<=money;j++){
+                    dp[j] = Math.max(dp[j],dp[j-candies.get(i).price]+candies.get(i).cal);
+                    maxn = Math.max(maxn,dp[j]);
+                }
+            }
+            sb.append(maxn+"\n");
+        }
+        System.out.println(sb);
+    }
+
+}
