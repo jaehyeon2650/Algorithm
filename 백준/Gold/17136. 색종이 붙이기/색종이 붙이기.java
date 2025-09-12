58fd291624dd9deead37d60325deea32fa9505a5
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-09-12 09:05:59 +0900

    [Gold II] Title: 색종이 붙이기, Time: 200 ms, Memory: 22340 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -1,77 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static int n = 10;
-    public static int[][] a = new int[11][11];
+    public static int[][] graph = new int[11][11];
     public static int[] counts = new int[6];
     public static int maxn = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 1; i <= 10; i++) {
-            String s = br.readLine();
-            String[] s1 = s.split(" ");
-            for (int j = 1; j <= 10; j++) {
-                a[i][j] = Integer.parseInt(s1[j - 1]);
+        for (int i = 0; i < 10; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 10; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        go(1, 1, 0);
-        if (maxn == Integer.MAX_VALUE) {
+        go(0,0,0);
+        if(maxn==Integer.MAX_VALUE){
             System.out.println(-1);
-        } else {
+        }else{
             System.out.println(maxn);
         }
     }
 
-    public static void go(int y, int x, int cnt) {
-        if (y > n) {
-            maxn = Math.min(maxn, cnt);
+    private static void go(int y,int x,int cnt) {
+        if(cnt>=maxn) return;
+        if(x==10) {
+            go(y+1,0,cnt);
             return;
         }
-        if (x > n) {
-            go(y + 1, 0, cnt);
+        if(y==10){
+            maxn = Math.min(maxn,cnt);
             return;
         }
-        if (a[y][x] == 0) {
-            go(y, x + 1, cnt);
+        if(graph[y][x]==0) {
+            go(y,x+1,cnt);
+            return;
         }
-        for (int i = 1; i <= 5; i++) {
-            if (check(y, x, i)) {
-                if (counts[i] == 5) {
-                    continue;
-                }
-                draw(y, x, i, 0);
+
+        for(int i=5;i>=1;i--){
+            if(can(y,x,i) && counts[i]<5){
+                checkGraph(y,x,i,0);
                 counts[i]++;
-                go(y, x + 1, cnt + 1);
+                go(y,x+1,cnt+1);
                 counts[i]--;
-                draw(y, x, i, 1);
+                checkGraph(y,x,i,1);
             }
         }
     }
 
-    public static void draw(int y, int x, int size, int value) {
-        for (int i = y; i < y + size; i++) {
-            for (int j = x; j < x + size; j++) {
-                a[i][j] = value;
+    private static void checkGraph(int y, int x, int size, int num) {
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                graph[y+i][x+j]=num;
             }
         }
     }
 
-    public static boolean check(int y, int x, int size) {
-        if (y + size - 1 > n || x + size - 1 > n) {
-            return false;
-        }
-        for (int i = y; i < y + size; i++) {
-            for (int j = x; j < x + size; j++) {
-                if (a[i][j] == 0) {
-                    return false;
-                }
+    private static boolean can(int y,int x, int size) {
+        if(y+size-1>=10 || x+size-1>=10) return false;
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                if(graph[y+i][x+j]==0) return false;
             }
         }
         return true;
     }
-
 }
