2e5fde24ac8411624f6ab7cec460a800764646fd
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-09-19 09:18:39 +0900

    [Gold IV] Title: 팰린드롬?, Time: 892 ms, Memory: 258076 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -1,50 +1,62 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class Main {
-    public static int n;
-    public static int[] a;
-    public static int[][] dp=new int[2001][2001];
-    public static int m;
+
+    private static int n;
+    private static int m;
+    private static int[] arr;
+    private static int[][] dp;
+
     public static void main(String[] args) throws IOException {
-        for(int i=0;i<2001;i++){
-            Arrays.fill(dp[i],-1);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bf.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        arr = new int[n + 1];
+        dp = new int[n + 1][n + 1];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb=new StringBuffer();
-        n=Integer.parseInt(br.readLine());
-        a=new int[n+1];
-        String s=br.readLine();
-        String[] s1 = s.split(" ");
-        for(int i=0;i<n;i++){
-            a[i+1]=Integer.parseInt(s1[i]);
-        }
-        for(int i=1;i<=n;i++){
-            dp[i][i]=1;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=i;j<=n;j++){
-                if(dp[i][j]==-1) go(i,j);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = isPal(i, j);
             }
         }
-        m=Integer.parseInt(br.readLine());
-        for(int i=0;i<m;i++){
-            s=br.readLine();
-            s1 = s.split(" ");
-            sb.append(dp[Integer.parseInt(s1[0])][Integer.parseInt(s1[1])]+"\n");
+
+        m = Integer.parseInt(bf.readLine());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            sb.append(dp[start - 1][end - 1] - 1 + "\n");
         }
+
         System.out.println(sb);
     }
 
-    public static int go(int x,int y){
-        if(dp[x][y]!=-1) return dp[x][y];
-        int ret=1;
-        if(a[x]!=a[y]) ret=0;
-        else{
-            if(x+1<=y-1) ret=go(x+1,y-1);
+    private static int isPal(int start, int end) {
+        if (start > end) {
+            return 2;
         }
-        dp[x][y]=ret;
-        return ret;
+        if (dp[start][end] != 0) {
+            return dp[start][end];
+        }
+        int result = 0;
+        if (arr[start] != arr[end]) {
+            result = 1;
+        } else {
+            result = 2;
+            if (isPal(start+1, end-1) == 1) {
+                result = 1;
+            }
+        }
+        dp[start][end] = result;
+        return dp[start][end];
     }
 
 }
