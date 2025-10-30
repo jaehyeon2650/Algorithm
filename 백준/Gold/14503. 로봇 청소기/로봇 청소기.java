6183df7434208c64095931f56eb4fb73860cbc40
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-10-30 16:52:40 +0900

    [Gold V] Title: 로봇 청소기, Time: 104 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -3,81 +3,81 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
 public class Main {
 
     private static int n;
     private static int m;
-    private static boolean[][] visited;
-    private static int[][] arr;
-    private static int[] dx = {0, 1, 0, -1};
-    private static int[] dy = {-1, 0, 1, 0};
-    private static int direction;
-    private static int nowX;
-    private static int nowY;
-    private static int count;
+    private static int[][] graph;
+    private static int nowDirection;
+    private static int nowy;
+    private static int nowx;
+    private static int[] dx = {0,1,0,-1};
+    private static int[] dy = {-1,0,1,0};
+    private static int result = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        arr = new int[n + 1][m + 1];
-        visited = new boolean[n + 1][m + 1];
+        m= Integer.parseInt(st.nextToken());
+        graph = new int[n][m];
         st = new StringTokenizer(bf.readLine());
-        nowY = Integer.parseInt(st.nextToken());
-        nowX = Integer.parseInt(st.nextToken());
-        direction = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < n; i++) {
+        nowy = Integer.parseInt(st.nextToken());
+        nowx = Integer.parseInt(st.nextToken());
+        nowDirection = Integer.parseInt(st.nextToken());
+        for(int i=0;i<n;i++){
             st = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for(int j=0;j<m;j++){
+                graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        while (true) {
-            if (arr[nowY][nowX] == 0 && !visited[nowY][nowX]) {
-                visited[nowY][nowX] = true;
-                count++;
-            }
-            if (aroundDirty(nowX, nowY)) {
-                direction = (direction - 1 + 4) % 4;
-                int ny = nowY + dy[direction];
-                int nx = nowX + dx[direction];
-                if (nx >= 0 && ny >= 0 && nx < m && ny < n && arr[ny][nx] == 0 && !visited[ny][nx]) {
-                    nowX = nx;
-                    nowY = ny;
-                }
-            } else if (canGoBack(nowX, nowY, direction)) {
-                int opposite = (direction + 2) % 4;
-                nowX = nowX + dx[opposite];
-                nowY = nowY + dy[opposite];
-            } else {
-                break;
+        while(true){
+            clean();
+            if(thereIsDirty()){
+                go();
+            }else{
+                if(!goBack()) break;
             }
         }
-        System.out.println(count);
+        System.out.println(result);
     }
 
-    private static boolean aroundDirty(int x, int y) {
-        boolean dirty = false;
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx < 0 || ny < 0 || nx >= m || ny >= n) {
-                continue;
-            }
-            if (arr[ny][nx] == 0 && !visited[ny][nx]) {
-                dirty = true;
-                break;
-            }
+    private static void clean(){
+        if(graph[nowy][nowx]==0){
+            graph[nowy][nowx]=2;
+            result++;
         }
-        return dirty;
     }
 
-    private static boolean canGoBack(int x, int y, int direction) {
-        int opposite = (direction + 2) % 4;
-        int nx = x + dx[opposite];
-        int ny = y + dy[opposite];
-        return nx >= 0 && ny >= 0 && nx < m && ny < n && arr[ny][nx] != 1;
+    private static boolean goBack(){
+        int back = (nowDirection+2)%4;
+        int ny = nowy+dy[back];
+        int nx = nowx+dx[back];
+        if(ny<0||nx<0||ny>=n||nx>=m||graph[ny][nx]==1) return false;
+        nowy = ny;
+        nowx = nx;
+        return true;
     }
+
+    private static boolean thereIsDirty(){
+        for(int i=0;i<4;i++){
+            int ny = nowy +dy[i];
+            int nx = nowx + dx[i];
+            if(ny<0||nx<0||ny>=n||nx>=m||graph[ny][nx]!=0) continue;
+            return true;
+        }
+        return false;
+    }
+
+    private static void go(){
+        nowDirection = (nowDirection-1+4)%4;
+        int ny = nowy + dy[nowDirection];
+        int nx = nowx + dx[nowDirection];
+        if(ny<0||nx<0||ny>=n||nx>=m||graph[ny][nx]!=0) return;
+        nowy = ny;
+        nowx = nx;
+    }
+
 }
