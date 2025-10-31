8d19c68aa026558d28bb12cc6570b1b2fbfdbc89
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-10-31 15:53:34 +0900

    [Gold III] Title: 아기 상어, Time: 180 ms, Memory: 20872 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -1,95 +1,118 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+
 public class Main {
-    static class Shark implements Comparable<Shark> {
-        int x;
-        int y;
-        int distance;
-
-        public Shark(int x, int y, int distance) {
-            this.x = x;
-            this.y = y;
-            this.distance = distance;
-        }
-
-
-        @Override
-        public int compareTo(Shark o) {
-            if (distance != o.distance) {
-                return distance - o.distance;
-            }
-            if (y != o.y) {
-                return y - o.y;
-            }
-            return x - o.x;
-        }
-    }
 
     private static int n;
-    private static int[][] arr;
-    private static Shark shark;
-    private static int sharkShape = 2;
-    private static int result = 0;
+    private static int[][] graph;
+    private static int[][] visited;
+    private static int sharkY;
+    private static int sharkX;
     private static int eaten = 0;
-    private static int[] dx = {0, -1, 0, 1};
-    private static int[] dy = {-1, 0, 1, 0};
+    private static int size = 2;
+    private static int[] dx = {0,-1,0,1};
+    private static int[] dy = {-1,0,1,0};
+    private static int count = 0;
     private static Queue<Shark> eats = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(bf.readLine());
-        arr = new int[n + 1][n + 1];
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-                if (arr[i][j] == 9) {
-                    arr[i][j] = 0;
-                    shark = new Shark(j, i, 0);
+        graph = new int[n][n];
+        visited = new int[n][n];
+        clear();
+        StringTokenizer st;
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(bf.readLine());
+            for(int j=0;j<n;j++){
+                graph[i][j] = Integer.parseInt(st.nextToken());
+                if(graph[i][j]==9){
+                    sharkY = i;
+                    sharkX = j;
+                    graph[i][j] = 0;
                 }
             }
         }
 
-        bfs(shark.x, shark.y);
-        while (!eats.isEmpty()) {
+        bfs();
+        while(!eats.isEmpty()){
+            Shark shark = eats.poll();
+            Point next = shark.point;
             eaten++;
-            Shark eatFish = eats.poll();
-            result = result + eatFish.distance;
-            if (eaten == sharkShape) {
+            if(eaten==size){
+                size++;
                 eaten = 0;
-                sharkShape++;
             }
-            arr[eatFish.y][eatFish.x] = 0;
+            count += visited[next.y][next.x];
+            graph[next.y][next.x] = 0;
+            sharkY = next.y;
+            sharkX = next.x;
+            clear();
             eats.clear();
-            bfs(eatFish.x,eatFish.y);
+            bfs();
         }
-        System.out.println(result);
+        System.out.println(count);
     }
 
-    public static void bfs(int x, int y) {
-        int[][] visited = new int[n + 1][n + 1];
-        Queue<Shark> sharks = new PriorityQueue<>();
-        sharks.add(new Shark(x, y, 0));
-        visited[y][x] = 1;
-        while (!sharks.isEmpty()) {
-            Shark now = sharks.poll();
-            for (int i = 0; i < 4; i++) {
-                int ny = now.y + dy[i];
-                int nx = now.x + dx[i];
-                if (ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx] != 0 || arr[ny][nx] > sharkShape) {
-                    continue;
-                }
-                sharks.add(new Shark(nx, ny, now.distance + 1));
-                visited[ny][nx] = 1;
-                if (arr[ny][nx] != 0 && arr[ny][nx] < sharkShape) {
-                    eats.add(new Shark(nx, ny, now.distance + 1));
-                }
+    private static void bfs(){
+        Queue<Point> queue = new ArrayDeque<>();
+        visited[sharkY][sharkX]=0;
+        queue.add(new Point(sharkY, sharkX));
+        while(!queue.isEmpty()){
+            Point cur = queue.poll();
+            int y = cur.y;
+            int x = cur.x;
+            if(graph[y][x]>0 && size>graph[y][x]) {
+                eats.add( new Shark(cur,visited[y][x]));
             }
+            for(int i=0;i<4;i++){
+                int ny = y+dy[i];
+                int nx = x+dx[i];
+                if(ny<0||nx<0||ny>=n||nx>=n||visited[ny][nx]!=-1||graph[ny][nx]> size) continue;
+                visited[ny][nx] = visited[y][x]+1;
+                queue.add(new Point(ny, nx));
+            }
+        }
+    }
+
+    private static void clear(){
+        for(int i=0;i<n;i++){
+            Arrays.fill(visited[i],-1);
+        }
+    }
+
+    static class Point{
+        int y;
+        int x;
+
+        public Point(final int y, final int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+
+    static class Shark implements Comparable<Shark>{
+        Point point;
+        int distance;
+
+        public Shark(final Point point, final int distance) {
+            this.point = point;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(final Shark o) {
+            if(distance != o.distance){
+                return distance - o.distance;
+            }
+            return point.y - o.point.y;
         }
     }
 }
