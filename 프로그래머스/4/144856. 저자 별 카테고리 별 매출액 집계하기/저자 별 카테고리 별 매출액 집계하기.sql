6d1b19118302be46f6686eaa8a93c062f92b372e
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-11-03 21:31:25 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,15 +1,23 @@
 -- 코드를 입력하세요
-with new_table as (select b.category,b.author_id,sum(sales*price) as total_sales from book_sales bs, book b
-where b.book_id=bs.book_id and month(bs.sales_date)=1 and year(bs.sales_date)=2022
-group by b.book_id,b.category, b.author_id)
+# select b.author_id, category from book b, author a
+# where b.author_id = a.author_id
+# group by b.author_id, category
 
-select a.author_id as author_id, author_name, category, sum(total_sales) as total_sales from new_table t,author a
-where t.author_id=a.author_id
-group by a.author_id, author_name, category
+# with new_table as (select book_id, category, author_id, price * (select sum(sales) from book_sales bs
+# where year(sales_date) = 2022 and month(sales_date) = 1 and bs.book_id = b.book_id) as total from book b)
+
+# select n.author_id, a.author_name, n.category, sum(total) as total_sales from new_table n, author a
+# where n.author_id = a.author_id
+# group by n.author_id, a.author_name, n.category
+# order by author_id desc, category desc
+
+# select book_id, category, author_id, price * (select sum(sales) from book_sales bs
+# where year(sales_date) = 2022 and month(sales_date) = 1 and bs.book_id = b.book_id) as total from book b
+
+select a.author_id, a.author_name, category, sum(price*sales) as total_sales from book b, author a, book_sales bs
+where b.book_id = bs.book_id and b.author_id = a.author_id
+and year(sales_date) = 2022 and month(sales_date) = 1
+group by a.author_id, a.author_name, b.category
 order by author_id asc, category desc
-
-
-
-
 
 
