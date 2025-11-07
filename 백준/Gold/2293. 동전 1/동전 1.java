7c3f9ced3885e7018904704f1f30e0986b3fc928
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-11-07 09:48:41 +0900

    [Gold IV] Title: 동전 1, Time: 108 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,25 +1,33 @@
-    import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    class Main {
+public class Main {
 
-        public static int n;
-        public static int k;
-        public static int[] dp;
-        public static void main(String[] args) throws IOException {
-            BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-            String s = bf.readLine();
-            String[] strings = s.split(" ");
-            n=Integer.parseInt(strings[0]);
-            k=Integer.parseInt(strings[1]);
-            dp=new int[k+1];
-            for(int i=0;i<n;i++){
-                int num=Integer.parseInt(bf.readLine());
-                if(num>k) continue;
-                dp[num]+=1;
-                for(int j=num;j<=k;j++){
-                    dp[j]+=dp[j-num];
-                }
-            }
-            System.out.println(dp[k]);
+    private static int n;
+    private static int m;
+    private static int[] coins;
+    private static int[] dp = new int[10004];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        coins = new int[n];
+        dp[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(bf.readLine());
         }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = coins[i]; j <= m; j++) {
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+        System.out.println(dp[m]);
     }
+}
